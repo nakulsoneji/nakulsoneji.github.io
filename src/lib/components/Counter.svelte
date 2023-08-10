8dc65214cdc035ecec288d65fbf90a2be2fd48c9
notk0ol <nakulsoneji@gmail.com>
2023-08-09 18:24:33 -0700

    counter testing

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  let count = 0;
+  let increment = () => {
+    if (count >= 0) {
+      count++;
+    }
+  }
+  let decrement = () => {
+    if (count > 0) {
+      count--;
+    }
+  }
+</script>
+
+<div class="red" style="width: 100px; text-align: center;">
+  <button on:click={increment}>+</button>
+  <br>
+  {count}
+  <br>
+  <button on:click={decrement}>-</button>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    import "$lib/assets/style.css";
+
+    export let data: LayoutData;
+</script>
+
+<nav>
+    navbar
+</nav>
+
+<slot />
